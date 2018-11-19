37c796402c13cfd76f4b543064f65cfa8d59419d
Fernanda <fernanda.r.tavares@avanade.com>
2018-11-19 13:36:18 -0200

    improves to validation message; test init

--- app/validation/productsValidation.js
+++ app/validation/productsValidation.js
@@ -1,10 +1,34 @@
 function productsValidation(req) {
     req.assert("titulo", "Titulo é obrigatório").notEmpty();
+    req.assert("titulo", "Titulo deve conter no máximo 45 caracteres").isLength({
+        max: 45
+    });
+    req.assert("descricao", "Descrição deve conter no máximo 250 caracteres").isLength({
+        max: 250
+    });
     req.assert("price", "Fromato inválido").isFloat();
     req.assert("price", "Fromato inválido").notEmpty();
     var erros = req.validationErrors();
 
-    return erros;
+    // return erros;
+    return formatError(erros);
+}
+
+function formatError(erros) {
+    var errorFormatted = [];
+    var obj = {};
+
+    erros.forEach(e => {
+        if (!obj[e.param]) {
+            obj[e.param] = [];
+            obj[e.param].push(e.msg);
+        } else {
+            obj[e.param].push(e.msg);
+        }
+    });
+    errorFormatted.push(obj);
+
+    return errorFormatted;
 }
 
 module.exports = function () {
---
+++ test/products.js
@@ -0,0 +1,5 @@
+describe('#ProdutosController', function () {
+    it('#listagem json', function () {
+        console.log("teste de verificacao de listagem json");
+    });
+});
