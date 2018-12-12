a0f0b7ad9d9c23728ab4696156f7218da9e5db11
Fernanda <fernanda.r.tavares@avanade.com>
2018-12-12 17:32:37 -0200

    Middleware added in express to 404 and 500

--- config/express.js
+++ config/express.js
@@ -1,23 +1,24 @@
 var express = require('express');
-var bodyParser = require('body-parser');
 var load = require('express-load');
+var bodyParser = require('body-parser');
 var expressValidator = require('express-validator');
 module.exports = function () {
     var app = express();
 
+    // -- configuring statics import
+    app.use(express.static('./app/public'));
+    // -- Configuring express
+    app.set('view engine', 'ejs');
+    app.set('views', './app/views');
+
     // -- Configuring body-parser
-    // support json encoded bodies
     app.use(bodyParser.json());
-    // support encoded bodies
+    // support json encoded bodies
     app.use(bodyParser.urlencoded({
         extended: true
     }));
     app.use(expressValidator());
-    app.use(express.static('./app/public'));
 
-    // -- Configuring express
-    app.set('view engine', 'ejs');
-    app.set('views', './app/views');
     console.log('config called');
 
     load('routes', {
@@ -27,5 +28,15 @@ module.exports = function () {
         .then('validation')
         .into(app);
 
+    app.use(function (req, res, next) {
+        res.status(404).render('erros/404');
+        next();
+    });
+
+
+    app.use(function (error, req, res, next) {
+        res.status(500).render('erros/500');
+        next();
+    });
     return app;
 }
