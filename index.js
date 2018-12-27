a40e133616ca5a4e4a1f3c472f89f809bbdf25a3
Fernanda <fernanda.r.tavares@avanade.com>
2018-12-27 20:50:16 -0200

    Changes made to fixe deploy in heroku

--- index.js
+++ index.js
@@ -4,16 +4,17 @@ var socketIo = require('socket.io')(http);
 
 app.set('io', socketIo);
 
+// var porta = process.env.PORT || 3000;
+// console.log('PORTA: %s', porta)
+// http.listen(porta, function () {
+//     console.log("Servidor rodando");
+// })
+
 var porta = process.env.PORT || 3000;
-console.log('PORTA: %s', porta)
-http.listen(porta, function () {
-    console.log("Servidor rodando");
-})
+var server = http.listen(porta, function () {
 
-// var server = http.listen(porta, function () {
+    var host = server.address().address;
+    var port = server.address().port;
 
-//     var host = server.address().address;
-//     var port = server.address().port;
-
-//     console.log('Example app listening at http://%s:%s', host, port);
-// });
+    console.log('Example app listening at http://%s:%s', host, port);
+});
